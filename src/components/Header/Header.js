b616281d4abcb55a99c2251afa26bd7579cf8c23
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-23 22:54:29 +0100

    Adding Newer component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt=""/>
+        </div>
+    );
+};
+
+export default Header;
