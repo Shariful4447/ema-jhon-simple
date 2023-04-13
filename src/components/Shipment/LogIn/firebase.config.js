bae442f580f83329766d5c240806aabd77242161
Shariful4447 <66253663+Shariful4447@users.noreply.github.com>
2023-04-13 05:34:28 +0200

    Update firebase.config.js

--- src/components/Shipment/LogIn/firebase.config.js
+++ src/components/Shipment/LogIn/firebase.config.js
@@ -1,11 +1,6 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC2DPzduAKL1OfHSk_D964P6NbPsjkvvP4",
-    authDomain: "ema-jhon-simple-480d1.firebaseapp.com",
-    projectId: "ema-jhon-simple-480d1",
-    storageBucket: "ema-jhon-simple-480d1.appspot.com",
-    messagingSenderId: "551084296857",
-    appId: "1:551084296857:web:f90a99d00fde64157f4608"
+
   };
 
   export default firebaseConfig;
